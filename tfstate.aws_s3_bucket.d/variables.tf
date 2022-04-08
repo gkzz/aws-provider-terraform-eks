83b95afe83be78d71746297273d1fd54a2ca404c
gkzz <38461277+gkzz@users.noreply.github.com>
2022-04-08 22:12:42 +0900

    AWS Providerを4.7.0にinit --upgradeで引き上げた。(applyはまだ)

---
+++ tfstate.aws_s3_bucket.d/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_access_key" {
+  default = null
+}
+
+variable "aws_secret_key" {
+  default = null
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "prefix" {
+  default = "gkzz-dev"
+}
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t2.small", "t3.small", "t2.medium"]
+  #default = ["t3.xlarge", "t3.2xlarge"]
+}
+
+variable "capacity_type" {
+  default = "SPOT"
+  #default = "ON_DEMAND"
+}
+
+variable "node_scaling_config" {
+  type = map(string)
+  default = {
+    desired_size = 3
+    max_size     = 4
+    min_size     = 2
+  }
+}
